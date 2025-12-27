44086932a46fe6722a77b084f33c84b273157f08
dhzdhd <dhzdhd@proton.me>
2025-12-27 22:50:56 +0530

    Add search box

--- composeApp/src/commonMain/kotlin/dev/dhzdhd/altivion/Greeting.kt
+++
@@ -1,9 +0,0 @@
-package dev.dhzdhd.altivion
-
-class Greeting {
-    private val platform = getPlatform()
-
-    fun greet(): String {
-        return "Hello, ${platform.name}!"
-    }
-}
--- composeApp/src/commonMain/kotlin/dev/dhzdhd/altivion/search/views/SearchView.kt
+++ composeApp/src/commonMain/kotlin/dev/dhzdhd/altivion/search/views/SearchView.kt
@@ -2,14 +2,20 @@ package dev.dhzdhd.altivion.search.views
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.ElevatedButton
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.text.input.TextFieldLineLimits
+import androidx.compose.foundation.text.input.rememberTextFieldState
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
 import dev.dhzdhd.altivion.search.viewmodels.SearchViewModel
 
 @Composable
@@ -18,9 +24,14 @@ fun SearchView(viewModel: SearchViewModel, contentPadding: PaddingValues) {
         modifier = Modifier.background(Color(0, 0, 0, 0))
             .padding(contentPadding).fillMaxSize(),
     ) {
-        ElevatedButton(onClick = {
-        }) {
-            Text("Click")
+        Column {
+            OutlinedTextField(
+                state = rememberTextFieldState(),
+                lineLimits = TextFieldLineLimits.SingleLine,
+                label = { Text("ICAO | IATA | Airport | Airplane") },
+                modifier = Modifier.fillMaxWidth().padding(horizontal = 16.dp)
+            )
+            LazyColumn { }
         }
     }
 }
