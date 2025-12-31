bde457b3ab39ac5fe3a40ae296f49591edce0383
dhzdhd <dhzdhd@proton.me>
2025-12-31 11:41:48 +0530

    Remove bottom sheet buttons and clean imports

--- composeApp/src/commonMain/kotlin/dev/dhzdhd/altivion/home/components/InteractiveMap.kt
+++ composeApp/src/commonMain/kotlin/dev/dhzdhd/altivion/home/components/InteractiveMap.kt
@@ -3,7 +3,6 @@ package dev.dhzdhd.altivion.home.components
 import altivion.composeapp.generated.resources.Res
 import altivion.composeapp.generated.resources.plane
 import androidx.compose.foundation.BorderStroke
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
@@ -13,26 +12,19 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.absoluteOffset
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.wrapContentSize
-import androidx.compose.foundation.layout.wrapContentWidth
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.GridItemSpan
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.ButtonColors
-import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.CircularProgressIndicator
-import androidx.compose.material3.ElevatedButton
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.HorizontalDivider
-import androidx.compose.material3.Label
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.ModalBottomSheet
 import androidx.compose.material3.Surface
@@ -51,16 +43,11 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.draw.clipToBounds
-import androidx.compose.ui.draw.rotate
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.ImageBitmap
-import androidx.compose.ui.graphics.painter.BitmapPainter
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.em
 import androidx.compose.ui.unit.sp
@@ -68,13 +55,9 @@ import arrow.core.Either
 import arrow.core.getOrElse
 import arrow.core.toOption
 import coil3.compose.AsyncImage
-import com.fleeksoft.ksoup.nodes.Range
 import com.mohamedrejeb.calf.permissions.ExperimentalPermissionsApi
 import com.mohamedrejeb.calf.permissions.Permission
-import com.mohamedrejeb.calf.permissions.isGranted
-import com.mohamedrejeb.calf.permissions.isNotGranted
 import com.mohamedrejeb.calf.permissions.rememberPermissionState
-import dev.dhzdhd.altivion.common.AppError
 import dev.dhzdhd.altivion.common.Value
 import dev.dhzdhd.altivion.home.models.Airplane
 import dev.dhzdhd.altivion.home.repositories.AirplaneImage
@@ -82,9 +65,7 @@ import dev.dhzdhd.altivion.home.services.HomeService
 import kotlinx.coroutines.launch
 import kotlinx.serialization.json.Json
 import org.jetbrains.compose.resources.painterResource
-import org.jetbrains.compose.ui.tooling.preview.Preview
 import org.koin.compose.koinInject
-import org.maplibre.compose.camera.CameraPosition
 import org.maplibre.compose.camera.rememberCameraState
 import org.maplibre.compose.expressions.dsl.Feature.get
 import org.maplibre.compose.expressions.dsl.asNumber
@@ -93,7 +74,6 @@ import org.maplibre.compose.expressions.dsl.image
 import org.maplibre.compose.expressions.dsl.plus
 import org.maplibre.compose.expressions.value.IconRotationAlignment
 import org.maplibre.compose.layers.SymbolLayer
-import org.maplibre.compose.location.DesiredAccuracy
 import org.maplibre.compose.map.MapOptions
 import org.maplibre.compose.map.MaplibreMap
 import org.maplibre.compose.map.OrnamentOptions
@@ -109,8 +89,6 @@ import org.maplibre.compose.util.ClickResult
 import org.maplibre.spatialk.geojson.Feature
 import org.maplibre.spatialk.geojson.FeatureCollection
 import org.maplibre.spatialk.geojson.Point
-import org.maplibre.compose.location.rememberDefaultLocationProvider
-import org.maplibre.spatialk.geojson.Position
 
 private val AirplaneStateSaver = Saver<Airplane?, String>(
     save = { Json.encodeToString(it) },
@@ -253,7 +231,8 @@ fun AirplaneInfoBottomSheet(
     }
 
     ModalBottomSheet(
-        onDismissRequest = { openBottomSheetState.value = false }, sheetState = bottomSheetState
+        onDismissRequest = { openBottomSheetState.value = false },
+        sheetState = bottomSheetState,
     ) {
         Column(
             modifier = Modifier.padding(all = 16.dp).scrollable(
@@ -269,19 +248,6 @@ fun AirplaneInfoBottomSheet(
                 FlightMetricsSection(airplane)
             } else {
                 Text("Error in retrieving aircraft details")
-            }
-        }
-        Row(modifier = Modifier.align(Alignment.CenterHorizontally)) {
-            ElevatedButton(
-                colors = ButtonDefaults.elevatedButtonColors(
-                    containerColor = MaterialTheme.colorScheme.primary,
-                    contentColor = MaterialTheme.colorScheme.inversePrimary
-                ),
-                onClick = {}) {
-                Text("History")
-            }
-            ElevatedButton(onClick = {}) {
-                Text("Track")
             }
         }
     }
